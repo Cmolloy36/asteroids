7efcf710f5e792624bc5f0b55ba78cc5069a428a
Cmolloy36 <cdmolloy36@gmail.com>
2024-12-20 23:22:45 -0500

    Finished Ch2! Only one left!

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def main():
     # print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    player.Player.containers = (updatable,drawable)
+
     game_player = player.Player(x = constants.SCREEN_WIDTH / 2, 
         y = constants.SCREEN_HEIGHT / 2)
 
@@ -19,9 +24,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        for item in updatable:
+            item.update(dt)
+
         pygame.Surface.fill(screen, (0,0,0))
-        game_player.draw(screen)
+        
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
--- player.py
+++ player.py
@@ -3,6 +3,9 @@ import constants
 import pygame
 
 class Player(circleshape.CircleShape):
+    # containers = (updatable,drawable)
+    # could add containers here, but would need to create group instances here too... I think
+
     def __init__(self,x,y):
         super().__init__(x,y,constants.PLAYER_RADIUS)
         self.rotation = 0
@@ -14,3 +17,23 @@ class Player(circleshape.CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def rotate(self,dt):
+        self.rotation += dt*constants.PLAYER_TURN_SPEED
+
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self,dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * constants.PLAYER_SPEED * dt
