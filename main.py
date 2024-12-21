f6bbd0047f6a1795ea4cf4af571c65f0f96b85e6
Cmolloy36 <cdmolloy36@gmail.com>
2024-12-20 22:45:50 -0500

    Finished Ch2 L5!

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pygame
 import constants
+import player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     # print("Starting asteroids!")
     # print(f"Screen width: {constants.SCREEN_WIDTH}")
     # print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT))
-    
+
+    game_player = player.Player(x = constants.SCREEN_WIDTH / 2, 
+        y = constants.SCREEN_HEIGHT / 2)
+
     while True:
         # This makes the close button work
         for event in pygame.event.get():
@@ -15,7 +21,10 @@ def main():
                 return
             
         pygame.Surface.fill(screen, (0,0,0))
+        game_player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
     
 if __name__ == "__main__":
     main()
